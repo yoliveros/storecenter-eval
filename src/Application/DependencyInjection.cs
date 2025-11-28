ce4e27a9c0593a5b474141ee3d7655cf4e7f9a1b
yoliveros <28690224+yoliveros@users.noreply.github.com>
2025-11-28 18:25:58 -0400

    Dependency injection for the application layer

--- src/Application/DependencyInjection.cs
+++ src/Application/DependencyInjection.cs
@@ -1,11 +1,22 @@
-using Microsoft.Extensions.Hosting;
+using Domain.Entities;
+using Application.DTOs;
+using Microsoft.Extensions.DependencyInjection;
+using Application.Services;
 
-namespace Microsoft.Extensions.DependencyInjection;
+namespace Application;
 
 public static class DependencyInjection
 {
-    public static void AddApplicationServices(this IHostApplicationBuilder builder)
+    public static IServiceCollection AddApplication(this IServiceCollection services)
     {
+        services.AddAutoMapper(cfg =>
+        {
+            cfg.CreateMap<Reservation, ReservationDTO>();
+            cfg.CreateMap<Room, RoomDTOs>();
+        });
 
+        services.AddScoped<IReservationService, ReservationService>();
+
+        return services;
     }
 }
--- src/Web/Program.cs
+++ src/Web/Program.cs
@@ -1,3 +1,4 @@
+using Application;
 using Scalar.AspNetCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -8,13 +9,16 @@ builder.Services.AddOpenApi();
 
 builder.Services.AddControllers();
 
+builder.Services
+    .AddApplication();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-  app.MapOpenApi();
-  app.MapScalarApiReference();
+    app.MapOpenApi();
+    app.MapScalarApiReference();
 }
 
 app.UseHttpsRedirection();
